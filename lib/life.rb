7b70c305d4abc9e32533176dd282ee7d44d1ab73
jeff <jeff.chun.yinli@gmail.com>
2015-07-12 15:22:29 -0400

    added display board method just like friday dojo

--- lib/life.rb
+++ lib/life.rb
@@ -1,2 +1,25 @@
 class Life
+
+  def initialize(number_of_rows, number_of_columns)
+    @board = Array.new(number_of_rows) { Array.new(number_of_columns) }
+    @number_of_columns = number_of_columns
+  end
+
+  def display
+    display = ""
+
+    @board.each do  |row|
+      row.each_with_index do |space, index|
+        display += '.'
+
+        if @number_of_columns > index + 1
+          display += ' '
+        else
+          display += "\n"
+        end
+      end
+    end
+
+    display
+  end
 end
--- spec/life_spec.rb
+++ spec/life_spec.rb
@@ -2,5 +2,8 @@ require 'spec_helper.rb'
 
 describe Life do
 
-
+  it 'should print an empty board' do
+    life = Life.new(2,3)
+    expect(life.display).to eq(". . .\n. . .\n")
+  end
 end
