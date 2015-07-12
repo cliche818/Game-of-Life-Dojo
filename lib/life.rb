e129988f4a1bb8cf42d556cefe95054ff46a9364
jeff <jeff.chun.yinli@gmail.com>
2015-07-12 16:06:36 -0400

    add the ability to check a cell's neighbour count

--- lib/life.rb
+++ lib/life.rb
@@ -3,6 +3,7 @@ class Life
   def initialize(number_of_rows, number_of_columns)
     @board = Array.new(number_of_rows) { Array.new(number_of_columns) { '.' } }
     @number_of_columns = number_of_columns
+    @number_of_rows = number_of_rows
   end
 
   def display
@@ -25,5 +26,45 @@ class Life
 
   def add_cell(x, y)
     @board[x][y] = '*'
+  end
+
+  def next_phase
+    @old_board = @board.dup
+
+
+    @board.each_with_index do |row, x|
+      row.each_with_index do |space, y|
+        if is_cell?(space) && neighbour_count(x, y) < 2
+          @board[i, j] = '.'
+        end
+      end
+    end
+
+  end
+
+  def is_cell?(space)
+    space == '*'
+  end
+
+  def neighbour_count(x, y)
+    count = 0
+
+    max_row_number = x + 1
+    max_column_number = y + 1
+
+    max_row_number.times do |x|
+      max_column_number.times do |y|
+
+        if not_out_of_bounds?(x, y) && is_cell?(@board[x][y])
+          count += 1
+        end
+      end
+    end
+
+    count
+  end
+
+  def not_out_of_bounds?(x, y)
+    x < @number_of_rows || y < @number_of_columns
   end
 end
--- spec/life_spec.rb
+++ spec/life_spec.rb
@@ -16,5 +16,34 @@ describe Life do
       @life.add_cell(1,1)
       expect(@life.display).to eq(". . .\n. * .\n")
     end
+
+    it 'should add a cell to every space on the board' do
+      2.times do |x|
+        3.times do |y|
+          @life.add_cell(x, y)
+        end
+      end
+
+      expect(@life.display).to eq("* * *\n* * *\n")
+    end
   end
+
+  describe 'neighbour_count' do
+    it 'the cell should have a neighbour count of 2' do
+      life = Life.new(3,3)
+      life.add_cell(1,1)
+      life.add_cell(0,1)
+      life.add_cell(2,1)
+      expect(life.neighbour_count(1,1)).to eq(2)
+    end
+  end
+
+  describe 'next_phase' do
+    xit 'should got to the next generation where cell [1,1] dies because fewer than 2 neighbours' do
+      @life.add_cell(1,1)
+      @life.next_phase
+      expect(@life.display).to eq(". . .\n. . .\n")
+    end
+  end
+
 end
